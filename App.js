f0432753b29cc80f0ce694d12fe8e041d2fbe8c5
Siam <shahidulmd392@gmail.com>
2021-06-03 00:37:04 +0600

    try to jsx and some of code web and native

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import "./App.css";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text class ="my-text" style={{color:'red', fontSize:'40px'}}>Welcome To My Apps</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +13,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
   },
 });
