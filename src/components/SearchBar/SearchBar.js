4e2c5faceae816b882743a613f785f8c61841fef
Mijan Karim <mkatbd@gmail.com>
2021-06-03 13:55:14 +0600

    added another component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,36 +1,32 @@
-import React from 'react';
-import {
-    StyleSheet,
-    View,
-    TextInput
-  } from 'react-native';
-  
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import COLORS from '../../consts/colors';
+import React from "react";
+import { StyleSheet, View, TextInput } from "react-native";
+
+import Icon from "react-native-vector-icons/MaterialIcons";
+import COLORS from "../../consts/colors";
 
 const SearchBar = () => {
-    return (
-        <View style={style.searchInputContainer}>
-          <Icon name="search" size={30} style={{marginLeft: 20}} />
-          <TextInput
-            placeholder="Search"
-            style={{fontSize: 20, paddingLeft: 10}}
-          />
-        </View>
-    )
-}
+  return (
+    <View style={style.searchInputContainer}>
+      <Icon name="search" size={30} style={{ marginLeft: 20 }} />
+      <TextInput
+        placeholder="Search"
+        style={{ fontSize: 20, paddingLeft: 10 }}
+      />
+    </View>
+  );
+};
 
-const style = StyleSheet.create({ 
-    searchInputContainer: {
-        height: 50,
-        backgroundColor: COLORS.light,
-        marginTop: 15,
-        marginLeft: 20,
-        borderTopLeftRadius: 30,
-        borderBottomLeftRadius: 30,
-        flexDirection: 'row',
-        alignItems: 'center',
-      }
-  });
+const style = StyleSheet.create({
+  searchInputContainer: {
+    height: 50,
+    backgroundColor: COLORS.light,
+    marginTop: 15,
+    marginLeft: 20,
+    borderTopLeftRadius: 30,
+    borderBottomLeftRadius: 30,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
 
 export default SearchBar;
